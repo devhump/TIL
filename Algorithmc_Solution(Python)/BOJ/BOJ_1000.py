2a5ca9863f005f5f8a79f17da99a415ee0fdf47e
devhump <devhum7p@gmail.com>
2022-07-25 20:48:40 +0900

    7/25 BOJ Algorithm_solution

---
+++ Algorithmc_Solution(Python)/BOJ/BOJ_1000.py
@@ -0,0 +1,21 @@
+
+# 런타임 에러
+#
+# num = []
+# num_sum =0
+
+# num = map(int, input().split())
+
+# for i in range(len(num)):
+#     num_sum += i
+
+# print(num_sum)
+
+
+###############################################
+# pass 코드
+##############################################
+
+a, b = map(int, input().split())
+
+print(a+b)
---
+++ Algorithmc_Solution(Python)/BOJ/BOJ_2577.py
@@ -0,0 +1,18 @@
+# A = int(input())
+# B = int(input())
+# C = int(input())
+
+A, B, C = 150, 266, 427
+
+num = str(A * B * C) #17037300
+
+num_0_to_9 = str("0123456789")
+
+num_cnt_dict = {}
+
+for char in num_0_to_9:
+    num_cnt_dict[char] = num.count(char)
+
+for i in num_0_to_9:
+    print(num_cnt_dict[i])
+
