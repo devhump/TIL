5f969bb3b650820a59ab53e294ab59226945f8b7
devhump <devhum7p@gmail.com>
2022-08-25 23:46:48 +0900

    HPHK py_practice 1~23. clear
    quit()

--- python_practice/py_practice_15_1.py
+++ python_practice/py_practice_15_1.py
@@ -1,32 +1,19 @@
-# 문제 15. 문자의 위치 구하기
-# 문자열 word가 주어질 때, 해당 문자열에서 a 가 처음으로 등장하는 위치(index)를 출력해주세요.
-# a 가 없는 경우에는 -1을 출력해주세요.
+# 문제 15-1. 문자의 위치 구하기
+# 문자열 word가 주어질 때, 해당 문자열에서 a 의 모든 위치(index)를 출력해주세요.
 # find() index() 메서드 사용 금지
 
-# word = 'apple'
-# word = 'kiwi'
-
-word = 'banana'
+# word = 'apple' # 0
+# word = 'kiwi' # 
+# word = 'HappyHacking' # 1 6
+word = 'banana' # 1 3 5
 
 idx = -1 # 'a' 순서 카운팅을 위한 idx
-# yes_or_no = 0 # 해당 문자열이 'a'를 가지고 있는가 검증
-
-# for char in word:
-#     if char == 'a':
-#         yes_or_no = 1
-#         # a가 있으면 1, a가 없으면 0
 
 for char in word:
-    # if yes_or_no == 0:
-    #     print(-1)
-    #     break
-    #     # a가 없는 경우 break
 
     idx += 1
 
     if char == 'a':
         print(idx, end=' ')
-        # break
-        # a가 있는 경우 index 값을 출력
-    
-# 기존 15번 문제에서 불필요한 부분만 주석 처리 해줬음    
+
+print()
--- python_practice/py_practice_18.py
+++ python_practice/py_practice_18.py
@@ -1,6 +1,41 @@
-word = int('apple')
-# i = ['a', 'e', 'i', 'o', 'u' ]
-# for a in range(len(word)):
-#     if word == i:
-#         else word != i    
-# print(a)
+# 문제 18.  알파벳 등장 갯수 구하기
+# > 문자열 word가 주어질 때, `**Dictionary**`를 활용해서 
+# 해당 문자열에서 등장한 모든 알파벳 개수를 구해서 출력하세요.
+
+### Input
+# banana
+# ### Output
+# b 1
+# a 3
+# n 2
+
+# 각 알파벳의 아스키값 확인
+# print(ord('a')) #97
+# print(ord('z')) #122 # a~z 25차이
+# print(ord("A"), ord("Z")) # 65 , 90
+
+word = 'Banana'
+word = list(word)
+
+#a:z 까지를 키값으로 갖는 0인 딕셔너리 생성
+az_dic = {}
+for i in range(97, 122+1):
+    az_dic[chr(i)] = 0
+
+word_2 =''
+for char in word:
+    if 65 <= ord(char) <= 90: # 각 알파벳이 대문자라면~
+        char = chr(ord(char) + 32) # 소문자로 바꿔서
+        word_2 += char #소문자를 저장
+    else:
+        word_2 += char #알파벳 저장
+
+    az_dic[char] += 1 #알파벳별 딕셔너리 값 +1
+
+print(word_2)
+
+for char in word_2:
+    if az_dic[char] != 0:
+        print(char, az_dic[char]) #각 알바벳별 출력
+    
+    az_dic[char] = 0 # 한 단어 내 동일한 알바벳 중복 방지를 위해
