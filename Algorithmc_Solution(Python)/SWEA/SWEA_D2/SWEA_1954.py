1bb1116662c5e4ca5c4ea8599d8aa7da6fca1e65
devhum7p <devhum7p@gmail.com>
2022-09-27 17:04:54 +0900

    09.27 ORM_practice clear!

--- Algorithmc_Solution(Python)/SWEA/SWEA_D2/SWEA_1954.py
+++ Algorithmc_Solution(Python)/SWEA/SWEA_D2/SWEA_1954.py
@@ -7,17 +7,18 @@
 
 import sys
 sys.stdin = open("SWEA_1954_input.txt", "r")
+from pprint import pprint
 
-
-T = int(input())
+# T = int(input())
+T = 1
 
 for t in range(T):
     num = int(input())
-    # num = 5
+    num = 6
 
     mx = [[0]*(num+2) for i in range(num+2)]
 
-
+    # * 로 된 테두리를 만들어 줌
     for i in range(num+2):
         mx[0][i] = '*'
         mx[i][0] = '*'
@@ -39,9 +40,16 @@ for t in range(T):
         elif mx[x+1][y] != 0 and mx[x][y+1] != 0 and mx[x][y-1] == 0: #아래&오른쪽 / 왼쪽
             mx[x][y] = i #왼쪽으로
             y -= 1
-        elif mx[x][y-1] != 0 and mx[x+1][y] != 0 and mx[x-1][y] == 0: #왼쪽&아래
+        elif mx[x][y-1] != 0 and mx[x+1][y] != 0 and mx[x-1][y] == 0: #왼쪽&아래 / 위로
             mx[x][y] = i #위로
             x -= 1
+
+        for i in range(num+2):
+            for j in range(num+2):
+                print(mx[i][j], end=' ')
+            if i != num+1:
+                print()
+        print()
  
     for i in range(num+2):
         for j in range(num+2):
@@ -50,8 +58,7 @@ for t in range(T):
 
     print(f'#{t+1}')
     for i in range(1, num+2):
-        # if i == 0:
-        #     print(f'#{t+1}')
+
         for j in range(1, num+2):
             if mx[i][j] != '*':
                 print(mx[i][j], end=' ')
