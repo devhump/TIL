8f3f1bcc198d9b446359e89fa67bef1b996363cf
devhump <66688033+devhump@users.noreply.github.com>
2023-01-11 19:03:56 +0900

    [Silver IV] Title: 동전 0, Time: 2720 ms, Memory: 114976 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+coins_num, sum_result = map(int, input().split())
+
+coins_list = []
+for i in range(coins_num):
+    temp = int(input())
+
+    if temp > sum_result:
+        continue
+    else:
+        coins_list.append(temp)
+
+cnt = 0
+while sum_result != 0:
+
+    max_ = max(coins_list)
+    if max_ > sum_result:
+        coins_list.pop()
+        continue
+    else:
+        sum_result -= max_
+        cnt += 1
+
+print(cnt)
