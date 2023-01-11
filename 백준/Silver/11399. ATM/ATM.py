7b38ab3fb86aca2faca6ff4d92de96e4044911cc
devhump <66688033+devhump@users.noreply.github.com>
2023-01-12 00:59:56 +0900

    [Silver IV] Title: ATM, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,13 @@
+cnt = int(input())
+
+time_list = list(map(int, input().split()))
+
+time_list.sort()
+
+temp = 0
+total = 0
+for num in time_list:
+    temp += num
+    total += temp
+
+print(total)
