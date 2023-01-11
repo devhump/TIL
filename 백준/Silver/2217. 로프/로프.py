6e186d5b3d5ca080e84900370ea3ae7958fe901d
devhump <66688033+devhump@users.noreply.github.com>
2023-01-12 00:21:45 +0900

    [Silver IV] Title: 로프, Time: 208 ms, Memory: 125844 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -1,17 +1,13 @@
-lope_cnt = int(input())
+cnt = int(input())
 
-lope_len_list = []
-for i in range(lope_cnt):
+rope = []
+value = []
+for i in range(cnt):
+    rope.append(int(input()))
 
-    temp = int(input())
-    lope_len_list.append(temp)
+rope.sort(reverse=True)
 
-lope_len_list = sorted(lope_len_list)
-
-sum_list = []
-while lope_cnt > 0:
-    sum_list.append(lope_cnt * lope_len_list[0])
-    lope_cnt -= 1
-    lope_len_list.pop(0)
-
-print(max(sum_list))
+for i in range(cnt):
+    value.append(rope[i]*(i+1))
+    
+print(max(value))
