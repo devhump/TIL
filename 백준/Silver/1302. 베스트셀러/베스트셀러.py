8bb9df9bd47fbb0e088248d256598ef09ab48e7b
devhump <66688033+devhump@users.noreply.github.com>
2023-04-05 16:49:02 +0900

    [Silver IV] Title: 베스트셀러, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -0,0 +1,26 @@
+
+cnt = int(input())
+
+best_sell = {}
+for i in range(cnt):
+    temp = input()
+    if best_sell.get(temp, 0):
+        best_sell[temp] += 1
+    else: 
+        best_sell[temp] = 1
+
+
+max_cnt = 0 
+for v in best_sell.values():
+    if v >= max_cnt:
+        max_cnt = v
+
+most_best = []
+for k, v in best_sell.items():
+    if v == max_cnt:
+        most_best.append(k)
+
+most_best.sort()        
+
+# for i in range(len(most_best)):
+print(most_best[0])
