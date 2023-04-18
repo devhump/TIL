3e05f3cb6becfbee6dc1d7ca5c0602b234db5742
devhump <devhum7p@gmail.com>
2023-04-18 19:29:22 +0900

    04.18 19:29 on PC

--- Algorithm/BOJ/BOJ_15953 상금헌터.py
+++ Algorithm/BOJ/BOJ_15953 상금헌터.py
@@ -1,5 +1,11 @@
 # BOJ_15953 상금헌터
 
+# ! 시간 초과도 아니고, 왜 자꾸 에러가 뜨는지 모르겠다 알림.
+# ? elif 절 조건문이 안 먹히나 해서 수정,
+# ? str으로 출력해서 그런가 또 수정
+# ? 왜 틀린지 이해가 안되서 1시간 반 잡고있다가 포기.
+
+
 import sys
 sys.stdin = open("BOJ_15953_input.txt", "r")
 
@@ -27,13 +33,13 @@ for t in range(T):
 
     if rank[1] == 1:
         result += 512
-    elif rank[1] <= 3:
+    elif 1 < rank[1] <= 3:
         result += 256
-    elif rank[1] <= 7:
+    elif 3 < rank[1] <= 7:
         result += 128
-    elif rank[1] <= 15:
+    elif 7 < rank[1] <= 15:
         result += 64
-    elif rank[1] <= 31:
+    elif 15 < rank[1] <= 31:
         result += 32
     else:
         result += 0
@@ -46,7 +52,41 @@ for t in range(T):
     #         result += int(512//((2**i)))
     #         break
 
-    if result == 0:
-        print("0")
-    else:
-        print(str(result) +'0000')
+    print(str(result)+'0000')
+
+
+
+# ! 인터넷에서 찾은 정답 코드인데, 뭐가 다르지...
+# t = int(input())
+
+# def fastival1(score):
+#     if score == 1:
+#         return 5000000
+#     elif 1 < score <= 3:
+#         return 3000000
+#     elif 3 < score <= 6:
+#         return 2000000
+#     elif 6 < score <= 10:
+#         return 500000
+#     elif 10 < score <= 15:
+#         return 300000
+#     elif 15 < score <= 21:
+#         return 100000
+#     else:
+#         return 0
+# def fastival2(score):
+#     if score == 1:
+#         return 5120000
+#     elif 1 < score <= 3:
+#         return 2560000
+#     elif 3 < score <= 7:
+#         return 1280000
+#     elif 7 < score <= 15:
+#         return 640000
+#     elif 15 < score <= 31:
+#         return 320000
+#     else:
+#         return 0
+# for _ in range(t):
+#     first, second = map(int, input().split(' '))
+#     print(fastival1(first) + fastival2(second))
