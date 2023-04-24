a415a78084841a859de595f1371e03759dae0c3a
devhump <66688033+devhump@users.noreply.github.com>
2023-04-24 22:49:37 +0900

    [Bronze II] Title: 피시방 알바, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/1453. 피시방 알바/피시방 알바.py
@@ -0,0 +1,18 @@
+n = int(input())
+    
+temp_li = list(map(int, input().split()))
+
+pc_dict = {}
+cnt = 0
+for i in range(n):
+    tem = temp_li.pop()    
+    if pc_dict.get(tem, 0):
+        pc_dict[tem] += 1
+    else:
+        pc_dict[tem] = 1
+
+res = 0
+for k, v in pc_dict.items():
+    res += (v - 1)
+
+print(res) 
