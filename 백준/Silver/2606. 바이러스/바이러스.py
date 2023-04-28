038c5a9481028c09669da8fde9c98b4dd96cdac1
devhump <66688033+devhump@users.noreply.github.com>
2023-04-28 22:54:02 +0900

    [Silver III] Title: 바이러스, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,32 @@
+
+com = int(input())
+graph = [[] for _ in range(com+1)]
+
+for _ in range(int(input())):
+    s, e = map(int, input().split())
+    graph[s].append(e)
+    graph[e].append(s) 
+
+visited = [False] * (com+1)
+# print(visited)
+
+
+def dfs(start):
+    cnt = 0
+    stack = [start]
+    visited[start] = True
+
+    while stack:
+        # print(f'stack: {stack}')
+        
+        cur = stack.pop()
+
+        for adj in graph[cur]:
+            if not visited[adj]:
+                visited[adj] = True
+                stack.append(adj)
+                cnt += 1
+
+    print(cnt)
+
+dfs(1)
