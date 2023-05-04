8b59c9dc54cbd62a8650f833fb4f73174a0e06ee
devhump <66688033+devhump@users.noreply.github.com>
2023-05-04 12:11:23 +0900

    [Silver III] Title: 킹, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.py
+++ 백준/Silver/1063. 킹/킹.py
@@ -1,33 +1,28 @@
-# BOJ_1063_google
+king, stone, n = input().split()
+n = int(n)
+king = [ord(king[0]) - ord("A") + 1, int(king[1])]
+stone = [ord(stone[0]) - ord("A") + 1, int(stone[1])]
 
-king, stone, N = input().split()
-k = list(map(int, [ord(king[0]) - 64, king[1]]))
-s = list(map(int, [ord(stone[0]) - 64, stone[1]]))
+move_types = ["R", "L", "B", "T", "RT", "LT", "RB", "LB"]
+dx = [1, -1, 0, 0, 1, -1, 1, -1]
+dy = [0, 0, -1, 1, 1, 1, -1, -1]
 
-#이때 k와 s는 [1,1] [8,8]
+for _ in range(n):
+    move = input()
+    idx = move_types.index(move) # todo index()를 통한 인덱스 접근!
 
-# 딕셔너리 (이동 타입에 따라 dx와 dy 설정)
-move = {'R': [1, 0], 'L': [-1, 0], 'B': [0, -1], 'T': [0, 1], 'RT': [1, 1], 'LT': [-1, 1], 'RB': [1, -1], 'LB': [-1, -1]}
+    king_delta = [king[0] + dx[idx], king[1] + dy[idx]]
+    stone_delta = [stone[0] + dx[idx], stone[1] + dy[idx]]
 
-# 움직이는 횟수 만큼 실행
-for _ in range(int(N)): # ! 처음에는 일괄 str로 받고, 필요할 때 형 변환!
-    m = input() #지금 이동
-    
-    # 움직였을 경우의 위치 : nx, ny
-    nx = k[0] + move[m][0] # todo 딕셔너리를 통한 이동이라니    
-    ny = k[1] + move[m][1] # todo 오오... 신선하다
-    
-    # 킹 조건 검사
-    if 0 < nx <= 8 and 0 < ny <= 8:
-    	# 돌 위에 얹히는가?
-        if nx == s[0] and ny == s[1]:
-            sx = s[0] + move[m][0]
-            sy = s[1] + move[m][1]
-            # 돌 조건 검사
-            if 0 < sx <= 8 and 0 < sy <= 8:
-                k = [nx, ny] # 킹 이동
-                s = [sx, sy] # 돌 이동 
+    if 1 <= king_delta[0] <= 8 and 1 <= king_delta[1] <= 8:
+        if king_delta == stone: # * 돌 위에 얹히면!
+            if 1 <= stone_delta[0] <= 8 and 1 <= stone_delta[1] <= 8:
+                king = king_delta
+                stone = stone_delta
         else:
-            k = [nx, ny] # 킹만 이동
-print(f'{chr(k[0] + 64)}{k[1]}')
-print(f'{chr(s[0] + 64)}{s[1]}')
+            king = king_delta
+
+# ! 어렵게 아스키 코드 찾고 → 변환하고 → 계산하고 하는 것 보다
+# ! 훨씬 알아보기 쉽고 편한 것 같다. 
+print(chr(king[0] + ord("A") - 1) + str(king[1])) 
+print(chr(stone[0] + ord("A") - 1) + str(stone[1]))
