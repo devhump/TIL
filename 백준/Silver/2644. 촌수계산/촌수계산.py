dacfb6527e3eb4e5ad8137477924d938d52622e8
devhump <66688033+devhump@users.noreply.github.com>
2023-05-11 20:03:13 +0900

    [Silver II] Title: 촌수계산, Time: 136 ms, Memory: 114436 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,37 +1,34 @@
-
 from collections import deque
-
 
 def bfs(start):
     queue = deque([start])
     visited[start] = True
+    cnt = 1
     while queue:
-        v = queue.popleft()
-        
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                # todo 부모노드가 갖는 노드에 1을 더하는 계산
-                res[i] = res[v] + 1 # todo 이게 포인트 구나 
-                visited[i] = True
+        x = queue.popleft()
+        cnt += 1
+        for adj in graph[x]:
+            if not visited[adj]:
+                visited[adj] = True
+                queue.append(adj)
+                ans[adj] = ans[x] + 1
 
-num = int(input())
-a,b = map(int, input().split())
+n = int(input())
+a, b = map(int, input().split())
+m = int(input())
 
-graph = [[] for _ in range(num+1)]
-for _ in range(int(input())):
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
     v1, v2 = map(int, input().split())
+
     graph[v1].append(v2)
     graph[v2].append(v1)
 
-visited = [False] * (num+1)
-res = [0] * (num+1) # todo 여기서 촌수 리스트를 만들면서 0으로 초기화 
-
+visited = [False] * (n+1)
+ans = [0] * (n+1)
 bfs(a)
 
-# print(res)
-
-if res[b] > 0:
-    print(res[b])
-else:
+if ans[b] == 0:
     print(-1)
+else:
+    print(ans[b])
