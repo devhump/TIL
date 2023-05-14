39b858d5b9fde0dc8f15fc0a2c89fc8ceb19f506
devhump <66688033+devhump@users.noreply.github.com>
2023-05-14 15:05:16 +0900

    [Silver I] Title: 스타트링크, Time: 472 ms, Memory: 70812 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,32 +1,29 @@
 from collections import deque
 
+def bfs(n, check):
+    queue = deque([n])
+    while queue:
 
-def bfs(start):
-    q = deque([start])
+        x = queue.popleft()
 
-    cnt = [-1] * f
-    cnt[start] = 0
+        if x == G:
+            return visited[x]
+        for nx in (x + U, x - D):
+            if 0 < nx <= F and not visited[nx] and (nx != S):
+                visited[nx] = visited[x] + 1
+                queue.append(nx)
+    return -1
 
-    while q:
-        cur_floor = q.popleft()
+F, S, G, U, D = map(int, input().split())
 
-        if cur_floor == g - 1:			# 도착하면 현재까지 누른 횟수를 반환
-            return cnt[cur_floor]
+visited = [0] *(F+1)
+check = False 
 
-        for i in range(2):
-            if i == 0:
-                next_floor = cur_floor + u
-            else:
-                next_floor = cur_floor - d
+ans = bfs(S, check)
 
-            if not 0 <= next_floor < f or cnt[next_floor] != -1:# 범위, 방문 여부 체크
-                continue
-
-            q.append(next_floor)
-            cnt[next_floor] = cnt[cur_floor] + 1
-
-    return 'use the stairs'				# 탐색을 다 해도 도착 못하면 문자열 반환
-
-
-f, s, g, u, d = map(int, input().split())
-print(bfs(s - 1))
+if ans >= 0:
+	print(ans)
+else:
+	print("use the stairs")
+        
+# print(visited)
