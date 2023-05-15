4b1f5701a31ff2167f93cde5cfd6a855753a7ae6
devhump <66688033+devhump@users.noreply.github.com>
2023-05-15 16:57:10 +0900

    [D2] Title: 가랏! RC카!, Time: 176 ms, Memory: 58,512 KB -BaekjoonHub

---
+++ SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
@@ -0,0 +1,27 @@
+
+T = int(input())
+
+for t in range(T):
+
+    dist = 0   
+    n = int(input())
+    speed = 0
+    for i in range(n):
+        cmd = list(map(int, input().split()))
+
+        if len(cmd) == 2:
+            if cmd[0] == 1: # 가속이면
+                dist += speed + cmd[1] # 현재거리 구하고
+                speed += cmd[1] # 속도 조정
+            elif cmd[0] == 2: # 감속이면
+                temp_spd = speed - cmd[1]
+                if temp_spd <= 0:
+                    temp_spd = 0
+                dist += temp_spd # 현재거리 구하고
+                speed -= cmd[1] # 속도조정
+                if speed <= 0:
+                    speed = 0
+        else: # 속도 유지면
+            dist += speed
+
+    print(f'#{t+1} {dist}')
