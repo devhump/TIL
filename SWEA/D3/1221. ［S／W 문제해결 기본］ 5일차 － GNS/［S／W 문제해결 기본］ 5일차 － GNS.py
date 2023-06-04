fe5a78e1cffadca98297814b55eca947fa14b80b
devhump <66688033+devhump@users.noreply.github.com>
2023-06-04 23:26:31 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 226 ms, Memory: 64,956 KB -BaekjoonHub

---
+++ SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
@@ -0,0 +1,44 @@
+planet = {
+    "ZRO" : 0, 
+    "ONE" : 1,
+    "TWO" : 2,
+    "THR" : 3, 
+    "FOR" : 4, 
+    "FIV" : 5,
+    "SIX" : 6,
+    "SVN" : 7,
+    "EGT" : 8,
+    "NIN" : 9
+    }
+re_planet = {
+    0 : "ZRO",
+    1 : "ONE",
+    2 : "TWO",
+    3 : "THR", 
+    4 : "FOR", 
+    5 : "FIV",
+    6 : "SIX",
+    7 : "SVN",
+    8 : "EGT",
+    9 : "NIN"
+    }
+
+T = int(input())
+
+for t in range(T):
+    a, b = input().split()
+    str_num = list(map(str, input().split()))
+
+    temp = []
+    for word in str_num:
+        temp.append(planet[word])
+
+    temp.sort()
+
+    res = []
+    for i in temp:
+        res.append(re_planet[i])
+
+    print(f'#{t+1}')
+    for i in range(int(b)):
+        print(res[i], end=" ")
