34f6ad904b4d7d34cccb97b1d7d667d45094d336
devhump <66688033+devhump@users.noreply.github.com>
2023-06-07 11:50:56 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 197 ms, Memory: 59,576 KB -BaekjoonHub

---
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+T = 10
+
+for t in range(T):
+    N = int(input())
+    encode = list(map(int, input().split()))
+    cmd_N = int(input())
+    cmd_str = list(input().split())
+    queue = deque(cmd_str)
+
+    for i in range(cmd_N):
+        
+        cmd = queue.popleft()
+
+        if cmd == "I":
+            idx = queue.popleft()
+            cnt = queue.popleft()
+
+            for k in range(int(cnt)):
+                temp = queue.popleft()
+                encode.insert(int(idx)+k, int(temp))
+
+        if cmd == "D":
+            idx = queue.popleft()
+            cnt = queue.popleft()
+
+            for k in range(int(cnt)):
+                del encode[int(idx)]
+
+    print(f'#{t+1}', end=" ")
+    print(*encode[:10])
