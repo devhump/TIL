e212a297209924e25c63d046c2f2e013ac7cc7bb
devhump <devhum7p@gmail.com>
2023-10-26 15:53:15 +0900

    codingApple/Nodejs_MongoDB part1 - 7

--- CodingApple/Node.js&MongoDB/forum/server.js
+++ CodingApple/Node.js&MongoDB/forum/server.js
@@ -7,21 +7,19 @@ const { MongoClient } = require("mongodb");
 
 let db;
 const url =
-  "mongodb+srv://ramy:monrj23@cluster0.meizkrl.mongodb.net/?retryWrites=true&w=majority";
-
+  "mongodb+srv://ramy:mongo123@cluster0.1obbp2g.mongodb.net/?retryWrites=true&w=majority";
 new MongoClient(url)
   .connect()
   .then((client) => {
     console.log("DB연결성공");
     db = client.db("forum");
+    app.listen(8080, () => {
+      console.log("http://localhost:8080 에서 서버 실행중");
+    });
   })
   .catch((err) => {
     console.log(err);
   });
-
-app.listen(8080, () => {
-  console.log("http://localhost:8080 에서 서버 실행중");
-});
 
 app.get("/", (요청, 응답) => {
   응답.sendFile(__dirname + "/index.html");
