c82dda6872b6b10d69fe146289315f263869f5f1
devhump <devhum7p@gmail.com>
2023-11-22 01:10:18 +0900

    23/11/22 01:10:10

--- CodingApple/Node.js&MongoDB/forum/server.js
+++ CodingApple/Node.js&MongoDB/forum/server.js
@@ -42,6 +42,7 @@ app.get("/about", function (요청, 응답) {
 app.get("/list", async (요청, 응답) => {
   let result = await db.collection("post").find().toArray();
   // 응답.send(result[0].title);
+  // console.log(result[0].title);
 
-  응답.render("list.ejs", { 글목록: result });
+  // 응답.render("list.ejs", { 글목록: result });
 });
